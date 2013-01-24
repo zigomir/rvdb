07d77e55f1ec988863fc38f5bb37469e321f3566
zigomir <zigomir@gmail.com>
2013-01-24 23:33:26 +0100

    cleaning git submodule files

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,14 +1,5 @@
 Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
-# Database servers
-include mysql
-
-# other dev tools like Node, ...
-include misc
-
-# casper js and phantom js for functional tests 
-include casperjs
-
 rbenv::install { 'vagrant':
   group => 'vagrant',
   home  => '/home/vagrant'
@@ -16,5 +7,7 @@ rbenv::install { 'vagrant':
 
 rbenv::compile { '1.9.3-p374':
   user => 'vagrant',
-  home  => '/home/vagrant'
+  home => '/home/vagrant'
 }
+
+include mysql, misc, casperjs
