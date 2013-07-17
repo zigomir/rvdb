b8d7aebdc933cd28cbea89fbf0f905bc478f37e7
zigomir <zigomir@gmail.com>
2013-07-17 20:33:51 +0200

    python for codecats

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,49 +4,10 @@ notice('Your conf from vagrantconfig.yml is: ')
 notice($conf)
 
 # include misc for parser functions to get configuration from facter variable $conf
+# and for installing all packages specified in vagrantconfig.yml
 include misc
-#include nodejs, phantomjs
 
-rbenv::install { 'vagrant':
-  group => 'vagrant',
-  home  => '/home/vagrant'
-}
-
-$ruby_version = get_fact($conf, 'ruby_version')
-rbenv::compile { $ruby_version:
-  user => 'vagrant',
-  home => '/home/vagrant'
-}
-
-$databases = get_fact_array($conf, 'databases')
-
-if fact_array_includes($databases, 'sqlite3') {
-  package { "libsqlite3-dev":
-    ensure => present
-  }
-}
-
-if fact_array_includes($databases, 'mysql') {
-  include mysql
-}
-
-if fact_array_includes($databases, 'postgres') {
-  class {'postgresql':  }
-  class {'postgresql::server':
-    listen => ['*'],
-    version => '9.1',
-    locale => 'en_US.UTF-8',
-    acl    => ['host all all 192.168.2.1/32 md5', ],
-  }
-  pg_user {'vagrant':
-    ensure   => present,
-    password => 'vagrant',
-    createdb   => true,
-    createrole => true,
-    superuser  => true
-  }
-
-  package { 'libpq-dev':
-    ensure => present
-  }
+exec { "pip install virtualenv":
+  unless  => "test -f /usr/local/bin/virtualenv",
+  command => "pip install virtualenv",
 }
--- modules/misc/manifests/init.pp
+++ modules/misc/manifests/init.pp
@@ -1,14 +1,8 @@
 class misc {
 
-  $toolsDirectory = "/home/vagrant/tools"
-
   $miscPackages = get_fact_array($conf, 'packages')
 
   package { $miscPackages:
     ensure => present
-  }
-
-  file { $toolsDirectory:
-    ensure => "directory"
   }
 }
